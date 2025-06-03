a40225414b6f9616563c28ab60e7ca222b327b91
virtualheet <heetvavadiya456@gmail.com>
2025-06-03 16:05:35 +0530

    Clean lib.rs and Add Basic Structure

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! # SimpMail
+//! 
+//! A temporary email management library for Rust beginners
+
+
+// client struct
+pub struct SimpMail {
+
+}
+
+// main implementation
+impl SimpMail {
+    pub fn new() -> Self {
+        SimpMail {
+            
+        }
+    }
+}
