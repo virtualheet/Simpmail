72649826a3d28d610343161b368abb57f5e4699c
virtualheet <heetvavadiya456@gmail.com>
2025-06-04 13:46:18 +0530

    Add EmailKeyword type with keyword retrieval functionality and update lib.rs to include types module

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 //! 
 //! A temporary email management library for Rust beginners
 
+pub mod types;
+
+pub use types::EmailKeyword;
+
 
 // client struct
 pub struct SimpMail {
@@ -15,4 +19,24 @@ impl SimpMail {
             
         }
     }   
+}
+
+
+#[cfg(test)]
+mod tests {
+      
+      use super::*;
+
+
+      #[test]
+      fn can_create_simpmail() {
+        let client = SimpMail::new();
+      }
+
+      #[test]
+      fn email_keyword_has_keywords() {
+        let love_keywords = EmailKeyword::Love.get_keywords();
+        assert!(love_keywords.contains(&"love"));
+        assert_eq!(love_keywords.len(), 5);
+      }
 }
--- src/types.rs
+++ src/types.rs
@@ -4,3 +4,15 @@ pub enum EmailKeyword {
     Gaming,
     Work,
 }
+
+impl EmailKeyword {
+    pub fn get_keywords(&self) -> Vec<&str> {
+        match self {
+            EmailKeyword::Love => vec!["love", "romance", "relationship", "rizz" , "dating"],
+            EmailKeyword::Gaming => vec!["gaming", "game", "play", "lol" ,"blow"],
+            EmailKeyword::Work => vec!["work", "job", "career", "job", "workout"],
+        }
+    }
+}
+
+
