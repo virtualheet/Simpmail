e24ab6a6c567b4cbb7f7f470743701f10c25043c
virtualheet <heetvavadiya456@gmail.com>
2025-06-05 23:39:36 +0530

    Add email generation functionality to SimpMail struct, utilizing HashMap for storage and UUID for unique token generation. Include tests for email generation.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 //! 
 //! A temporary email management library for Rust beginners
 
+use std::collections::HashMap;
+use rand::Rng;
+use uuid::Uuid;
+
 pub mod types;
 
 pub use types::EmailKeyword;
@@ -9,24 +13,36 @@ pub use types::EmailKeyword;
 
 // client struct
 pub struct SimpMail {
-
+ genrated_emails : HashMap<String, String>
 }
 
 // main implementation
 impl SimpMail {
     pub fn new() -> Self {
         SimpMail {
-            
+            genrated_emails : HashMap::new()
         }
-    }   
+    }  
+
+
+    pub fn generate_email(&mut self,keyword: EmailKeyword) -> String {
+       let keywords = keyword.get_keywords();
+       let selected = keywords[rand::thread_rng().gen_range(0..keywords.len())];
+
+       let random_num: u32 = rand::thread_rng().gen_range(1000..9999);
+       let email = format!("{}{}@example.com", selected, random_num);
+
+       let token = Uuid::new_v4().to_string();
+       self.genrated_emails.insert(email.clone(), token);
+
+       email
+      }
 }
 
 
 #[cfg(test)]
 mod tests {
-      
       use super::*;
-
 
       #[test]
       fn can_create_simpmail() {
@@ -39,4 +55,14 @@ mod tests {
         assert!(love_keywords.contains(&"love"));
         assert_eq!(love_keywords.len(), 5);
       }
+}
+
+
+#[test]
+fn can_generate_email() {
+    let mut client = SimpMail::new();
+    let email = client.generate_email(EmailKeyword::Love);
+    
+    assert!(email.contains("@example.com"));
+    assert!(email.contains("love") || email.contains("heart") || email.contains("sweet"));
 }
